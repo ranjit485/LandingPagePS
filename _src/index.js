7e49bb56fa06f6683598507e57ad718197377e3e
RanjitPatil <patilranjit485@gmail.com>
2025-01-17 11:48:40 +0530

    ..

--- _src/index.js
+++ _src/index.js
@@ -5,8 +5,8 @@ import { default as Tobi } from '@midzer/tobii'
 const tobi = new Tobi()
 
 // Remove the two following lines to remove the product hunt floating prompt
-import FloatingPrompt from 'producthunt-floating-prompt'
-FloatingPrompt({ name: 'Get Latest updates Join Community', url: 'https://chat.whatsapp.com/IqVjV79WV6P9WGlL605iem', bottom: '96px', width: '450px' })
+// import FloatingPrompt from 'producthunt-floating-prompt'
+// FloatingPrompt({ name: 'Get Latest updates Join Community', url: 'https://chat.whatsapp.com/IqVjV79WV6P9WGlL605iem', bottom: '96px', width: '450px' })
 
 // // Remove the following lines to remove the darkmode js
 // import Darkmode from 'darkmode-js'
